b7b6f6122a0ce45e40d96eb692753400f5760e5e
Jamie Beardslee <beardsleejamie@gmail.com>
2019-07-24 16:03:36 +1200

    some decent QoL fixes here and there, new colour scheme

---
+++ elpa/elpher-20190710.755/elpher-pkg.el
@@ -0,0 +1,12 @@
+(define-package "elpher" "20190710.755" "A friendly gopher client."
+  '((emacs "26"))
+  :keywords
+  '("comm" "gopher")
+  :authors
+  '(("Tim Vaughan" . "tgvaughan@gmail.com"))
+  :maintainer
+  '("Tim Vaughan" . "tgvaughan@gmail.com")
+  :url "https://github.com/tgvaughan/elpher")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
