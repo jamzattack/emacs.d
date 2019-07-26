9fbf7c17c3fbba18b3a3d20cb930a0ce4a4a5066
Jamie Beardslee <beardsleejamie@gmail.com>
2019-07-26 16:42:36 +1200

    Some major changes, including exwm and elisp-defined compilation for some major modes

--- elpa/xclip-1.9/xclip-pkg.el
+++ elpa/xclip-1.9/xclip-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "xclip" "1.9" "Copy&paste GUI clipboard from text terminal" 'nil :url "http://elpa.gnu.org/packages/xclip.html" :keywords '("convenience" "tools") :authors '(("Leo Liu" . "sdl.web@gmail.com")) :maintainer '("Leo Liu" . "sdl.web@gmail.com"))
+(define-package "xclip" "1.9" "Copy&paste GUI clipboard from text terminal" 'nil :authors '(("Leo Liu" . "sdl.web@gmail.com")) :maintainer '("Leo Liu" . "sdl.web@gmail.com"))
