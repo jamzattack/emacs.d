9ccffc6846bfedc3c4a9a241146bb5c9c2d9c144
Jamie Beardslee <beardsleejamie@gmail.com>
2019-11-13 00:31:38 +1300

    Added a line to load my font settings. Removed some unneeded stuff. Trying to minimise the contents of init.el

--- init.el
+++ init.el
@@ -1,48 +1,31 @@
+;; Make sure the default directory is correct
+(setq default-directory "/home/jdb/.emacs.d/")
 ;; This loads my config.org file
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(org-babel-load-file (concat default-directory "config.org"))
+;; This loads my fonts settings
+(load-file (concat default-directory "config/fonts.el"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#eeeeee" "#bcbcbc" "#8700af" "#d70087" "#d75f00" "#005faf" "#d75f00" "#878787"])
- '(ansi-term-color-vector
-   [unspecified "#eeeeee" "#bcbcbc" "#8700af" "#d70087" "#d75f00" "#005faf" "#d75f00" "#878787"] t)
  '(browse-url-browser-function 'eww-browse-url)
- '(custom-safe-themes
-   '("5a39d2a29906ab273f7900a2ae843e9aa29ed5d205873e1199af4c9ec921aaab" default))
  '(dired-listing-switches "-alh")
  '(exwm-input-prefix-keys
-   '("" "" "" ""
+   '([f8]
+     ""
+     [menu]
+     "" "" ""
      [134217848]
      [134217824]
      [134217766]
-     [134217786]
-     [134217761]
-     [8388724]))
+     [134217786]))
  '(exwm-workspace-number 4)
  '(exwm-workspace-switch-create-limit 4)
  '(global-company-mode t)
- '(notmuch-address-internal-completion '(received nil))
- '(notmuch-saved-searches
-   '((:name "inbox" :query "tag:inbox" :key "i")
-     (:name "unread" :query "tag:unread" :key "u")
-     (:name "flagged" :query "tag:flagged" :key "f")
-     (:name "sent" :query "tag:sent" :key "t")
-     (:name "drafts" :query "tag:draft" :key "d")
-     (:name "all mail" :query "*" :key "a")
-     (:name "university" :query "university")))
  '(package-selected-packages
-   '(god-mode slime vterm desktop-environment haskell-mode auto-complete auto-comp htmlize ox-gfm geiser powerline flycheck-lilypond flycheck pdf-tools xelb exwm xterm-color xclip which-key use-package try transmission switch-window smex rainbow-mode rainbow-delimiters pinentry notmuch mingus lorem-ipsum ido-vertical-mode elpher counsel company base16-theme))
+   '(openwith fish-completion god-mode slime vterm desktop-environment haskell-mode htmlize ox-gfm geiser flycheck-lilypond flycheck pdf-tools xelb exwm xterm-color xclip which-key use-package try transmission switch-window smex rainbow-mode rainbow-delimiters pinentry mingus lorem-ipsum ido-vertical-mode elpher counsel company base16-theme))
  '(show-paren-mode t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch-serif ((t (:foundry "    " :family "Go Mono")))))
 
 (provide 'init)
