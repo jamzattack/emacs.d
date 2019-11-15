f0408b7aef7765b660c7fe2405da1b22455a9d68
Jamie Beardslee <beardsleejamie@gmail.com>
2019-11-15 21:07:59 +1300

    fixed font loading

--- init.el
+++ init.el
@@ -1,9 +1,10 @@
-;; Make sure the default directory is correct
-(setq default-directory "/home/jdb/.emacs.d/")
+;; Turn this directory into a variable
+(setq emacs-config-directory (expand-file-name "~/.emacs.d/"))
 ;; This loads my config.org file
-(org-babel-load-file (concat default-directory "config.org"))
+(org-babel-load-file (concat emacs-config-directory "config.org"))
 ;; This loads my fonts settings
-(load-file (concat default-directory "config/fonts.el"))
+(global-set-key (kbd "C-z f") (lambda nil (interactive)
+                                    (load-file (concat emacs-config-directory "config/fonts.el"))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -25,7 +26,7 @@
  '(exwm-workspace-switch-create-limit 4)
  '(global-company-mode t)
  '(package-selected-packages
-   '(openwith fish-completion god-mode slime vterm desktop-environment haskell-mode htmlize ox-gfm geiser flycheck-lilypond flycheck pdf-tools xelb exwm xterm-color xclip which-key use-package try transmission switch-window smex rainbow-mode rainbow-delimiters pinentry mingus lorem-ipsum ido-vertical-mode elpher counsel company base16-theme))
+   '(exwm-surf openwith fish-completion god-mode slime vterm desktop-environment haskell-mode htmlize ox-gfm geiser flycheck-lilypond flycheck pdf-tools xelb exwm xterm-color xclip which-key use-package try transmission switch-window smex rainbow-mode rainbow-delimiters pinentry mingus lorem-ipsum ido-vertical-mode elpher counsel company base16-theme))
  '(show-paren-mode t))
 
 (provide 'init)
