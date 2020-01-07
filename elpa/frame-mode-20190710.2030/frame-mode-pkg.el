4c2fdeed95cdd3eecbed6fad45451b0008e633b1
Jamie Beardslee <beardsleejamie@gmail.com>
2020-01-07 15:45:41 +1300

    updated elpa packages

--- elpa/frame-mode-20190710.2030/frame-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "frame-mode" "20190710.2030" "Use frames instead of windows" '((s "1.9.0") (emacs "24.4")) :commit "ae2366969927c9f89ea07c999bef382b0b47cac1" :keywords '("frames") :authors '(("Ivan Malison" . "IvanMalison@gmail.com")) :maintainer '("Ivan Malison" . "IvanMalison@gmail.com") :url "https://github.com/IvanMalison/frame-mode")
--- elpa/xclip-1.9/xclip-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/jdb/.emacs.d/elpa/xclip-1.9/xclip.el  -*- no-byte-compile: t -*-
-(define-package "xclip" "1.9" "Copy&paste GUI clipboard from text terminal" 'nil :url "http://elpa.gnu.org/packages/xclip.html" :keywords '("convenience" "tools") :authors '(("Leo Liu" . "sdl.web@gmail.com")) :maintainer '("Leo Liu" . "sdl.web@gmail.com"))
