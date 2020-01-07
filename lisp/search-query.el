251cc93b5b7b99f35f75d9e3dc03b70df577380a
Jamie Beardslee <beardsleejamie@gmail.com>
2020-01-07 15:42:40 +1300

    Web search package, pretty boring but helps set some keybindings for specific sites.

--- lisp/search-query.el
+++ lisp/search-query.el
@@ -1,17 +1,16 @@
 (defun torrentz2-search (query)
-  (interactive (list (read-string "Torrent search: ")))
+  (interactive (list (read-string "Torrent: ")))
   (browse-url
    (concat "https://torrentz2.eu/search?f=" query)))
 
 (defun youtube-search (query)
-  (interactive (list (read-string "Youtube search: ")))
+  (interactive (list (read-string "Youtube: ")))
   (browse-url
    (concat "https://m.youtube.com/search?q=" query)))
 
 (defun wikipedia-search (query)
-  (interactive (list (read-string "Wikipedia search: ")))
+  (interactive (list (read-string "Wikipedia: ")))
   (browse-url
    (concat "https://wikipedia.org/wiki/" query)))
-
 
 (provide 'search-query)
