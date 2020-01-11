7e13f478a38bca34a5d606ba6d274b1c965fe412
Jamie Beardslee <beardsleejamie@gmail.com>
2020-01-12 00:59:57 +1300

    Got rid of messy pdf-tools stuff in custom theme

--- custom-theme.el
+++ custom-theme.el
@@ -1,22 +1,16 @@
 (deftheme custom
-  "Nothing but changing the background colour to off-white")
-(defun custom-theme-pdf-view ()
-  (pdf-info-setoptions
-   :render/foreground "black"
-   :render/background "#FFFFD0"
-   :render/usecolors t))
+  "Not much but changing the background colour to off-white")
 
-(add-hook 'pdf-view-mode-hook 'custom-theme-pdf-view)
-
-(let ((class '((class color) (min-colors 89))))
+(let ((class '((class color) (min-colors 89)))
+      (almost-white "#FFFFD0"))
   (custom-theme-set-faces
    'custom
-   `(default ((,class (:background "#FFFFD0" :foreground "black"))))
+   `(default ((,class (:background ,almost-white :foreground "black"))))
    `(border-color ((,class (:inherit 'default))))
+   `(fringe ((,class (:inherit 'default))))
    `(line-number-current-line ((,class (:inherit 'secondary-selection))))
    `(secondary-selection ((,class (:background "khaki1" :extend t))))
-   `(helm-source-header ((,class (:inherit 'bold))))
-   `(helm-selection-line ((,class (:inherit 'hl-line)))))
-  (setq pdf-view-midnight-colors '("white" . "black")))
+   `(scroll-bar ((,class (:foreground "peru" :background ,almost-white :box nil))))
+   `(helm-source-header ((,class (:inherit 'bold))))))
 
 (provide-theme 'custom)
