dcc9a4ac9a08c4f3cb9ad6fd222270f3ce8b3836
Jamie Beardslee <beardsleejamie@gmail.com>
2020-01-13 02:49:31 +1300

    updated ELPA packages

---
+++ elpa/edwin-20190818.2223/edwin-pkg.el
@@ -0,0 +1,10 @@
+(define-package "edwin" "20190818.2223" "Dynamic window manager"
+  '((emacs "25"))
+  :authors
+  '(("Alex Griffin" . "a@ajgrf.com"))
+  :maintainer
+  '("Alex Griffin" . "a@ajgrf.com")
+  :url "https://github.com/ajgrf/edwin")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/edwina-20190821.1850/edwina-pkg.el
@@ -0,0 +1,12 @@
+(define-package "edwina" "20190821.1850" "Dynamic window manager"
+  '((emacs "25"))
+  :keywords
+  '("convenience")
+  :authors
+  '(("Alex Griffin" . "a@ajgrf.com"))
+  :maintainer
+  '("Alex Griffin" . "a@ajgrf.com")
+  :url "https://github.com/ajgrf/edwina")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
