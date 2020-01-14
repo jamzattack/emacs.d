a5ab95253271b7e737ccfd006f72a97621ebcd6f
Jamie Beardslee <beardsleejamie@gmail.com>
2020-01-14 14:00:36 +1300

    setq swapped with custom-set-variables so that you can modify and the changes will work

--- lisp/custom-exwm-config.el
+++ lisp/custom-exwm-config.el
@@ -48,50 +48,68 @@ output."
 
 ;; Global keybindings.
 (defun custom-exwm-input-global-keys ()
-  (setq exwm-input-global-keys
-        `(
-          ;; 's-r': Reset (to line-mode).
-          ([?\s-r] . exwm-reset)
-          ;; 's-w': Switch workspace.
-          ([?\s-w] . exwm-workspace-switch)
-          ;; 's-&': Launch application.
-          ([?\s-&] . exwm-shell-command)
-          ;; 's-N': Switch to certain workspace.
-          ,@(mapcar (lambda (i)
-                      `(,(kbd (format "s-%d" i)) .
-                        (lambda ()
-                          (interactive)
-                          (exwm-workspace-switch-create ,i))))
-                    (number-sequence 0 4)))))
+  (custom-set-variables
+   `(exwm-input-global-keys
+     '(
+       ;; (,(kbd "s-j") . edwina-select-next-window)
+       ;; (,(kbd "s-k") . edwina-select-previous-window)
+       ;; (,(kbd "s-S-j") . edwina-swap-next-window)
+       ;; (,(kbd "s-J") . edwina-swap-next-window)
+       ;; (,(kbd "s-S-k") . edwina-swap-previous-window)
+       ;; (,(kbd "s-K") . edwina-swap-previous-window)
+       ;; (,(kbd "s-h") . edwina-dec-mfact)
+       ;; (,(kbd "s-l") . edwina-inc-mfact)
+       ;; (,(kbd "s-d") . edwina-dec-nmaster)
+       ;; (,(kbd "s-i") . edwina-inc-nmaster)
+       ;; (,(kbd "s-S-c") . edwina-delete-window)
+       ;; (,(kbd "s-C") . edwina-delete-window)
+       ;; (,(kbd "<s-RET>") . edwina-zoom)
+       ;; (,(kbd "<s-return>") . edwina-zoom)
+       ;; (,(kbd "<s-S-RET>") . edwina-clone-window)
+       ;; (,(kbd "<s-S-return>") . edwina-clone-window)
+       ;; ;; 's-r': Reset (to line-mode).
+       ([?\s-r] . exwm-reset)
+       ;; 's-w': Switch workspace.
+       ([?\s-w] . exwm-workspace-switch)
+       ;; 's-&': Launch application.
+       ([?\s-&] . exwm-shell-command)
+       ;; 's-N': Switch to certain workspace.
+       ,@(mapcar (lambda (i)
+                   `(,(kbd (format "s-%d" i)) .
+                     (lambda ()
+                       (interactive)
+                       (exwm-workspace-switch-create ,i))))
+                 (number-sequence 0 4))))))
 
 ;; Line-editing shortcuts
 (defun custom-exwm-input-simulation-keys ()
-  (setq exwm-input-simulation-keys
-        '( ; Basic movement
-          ([?\C-b] . [left])
-          ([?\C-f] . [right])
-          ([?\C-p] . [up])
-          ([?\C-n] . [down])
-          
-          ([?\M-f] . [C-right])
-          ([?\M-b] . [C-left])
+  (custom-set-variables
+   '(exwm-input-simulation-keys
+     '( ; Basic movement
+       ([?\C-b] . [left])
+       ([?\C-f] . [right])
+       ([?\C-p] . [up])
+       ([?\C-n] . [down])
+       
+       ([?\M-f] . [C-right])
+       ([?\M-b] . [C-left])
 
-          ([?\C-a] . [home])
-          ([?\C-e] . [end])
+       ([?\C-a] . [home])
+       ([?\C-e] . [end])
 
-          ([?\M-v] . [prior])
-          ([?\C-v] . [next])
+       ([?\M-v] . [prior])
+       ([?\C-v] . [next])
 
-          ;; Deleting text
-          ([?\C-d] . [delete])
-          ([?\C-k] . [S-end delete])
-          ([?\M-d] . [S-C-right delete])
-          ([?\M-\d] . [C-DEL])
+       ;; Deleting text
+       ([?\C-d] . [delete])
+       ([?\C-k] . [S-end delete])
+       ([?\M-d] . [S-C-right delete])
+       ([?\M-\d] . [C-DEL])
 
-          ;; clipboard/kill-ring
-          ([?\C-w] . [C-x])
-          ([?\M-w] . [C-c])
-          ([?\C-y] . [C-v]))))
+       ;; clipboard/kill-ring
+       ([?\C-w] . [C-x])
+       ([?\M-w] . [C-c])
+       ([?\C-y] . [C-v])))))
 
 (defun custom-exwm-config ()
   ;; Don't start with extra workspaces
