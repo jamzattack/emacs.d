4a0d638db4d20e6f5b396d415c1b57722853c887
Jamie Beardslee <beardsleejamie@gmail.com>
2020-01-14 14:39:49 +1300

    Added modes for arch package and systemd service/target files.

---
+++ lisp/arch-linux-settings.el
@@ -0,0 +1,6 @@
+(add-to-list 'auto-mode-alist '("\\/PKGBUILD\\'" . shell-script-mode))
+(add-to-list 'auto-mode-alist '("\\/\\.SRCINFO\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.service\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.target\\'" . conf-unix-mode))
+
+(provide 'arch-linux-settings)
