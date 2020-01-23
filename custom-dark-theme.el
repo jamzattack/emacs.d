a374c7593b76a041147d38361ea5a698efcbaeda
Jamie Beardslee <beardsleejamie@gmail.com>
2020-01-23 21:01:38 +1300

    Added blue mode-line in custom-dark-theme

--- custom-dark-theme.el
+++ custom-dark-theme.el
@@ -12,6 +12,7 @@
    `(line-number-current-line ((,class (:inherit 'secondary-selection))))
    `(secondary-selection ((,class (:background "gray9" :extend t))))
    `(scroll-bar ((,class (:foreground ,almost-white :background "black"))))
+   `(mode-line ((,class (:foreground "black" :background "sky blue"))))
    `(helm-source-header ((,class (:inherit 'bold))))))
 
 (provide-theme 'custom-dark)
