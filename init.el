15a6740db18fc85836bde8f6357ddd5272adadd4
Jamie Beardslee <beardsleejamie@gmail.com>
2020-02-02 13:53:39 +1300

    set load-prefer-newer, moved load-path modification to top of init.el

--- init.el
+++ init.el
@@ -1,8 +1,13 @@
+;; Prefer newer files rather than old byte-compiled ones.
+(setq load-prefer-newer t)
+
+;; add the lisp directory to `load-path'
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
 ;; Move custom settings to another file
 (setq custom-file (concat user-emacs-directory "custom.el"))
 
 ;; Load my real config, then the custom file
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'config.el)
 (load custom-file)
 
