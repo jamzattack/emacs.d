b14aa4f9408705109fe5d36f973237c3711194fe
Jamie Beardslee <beardsleejamie@gmail.com>
2020-02-05 14:26:10 +1300

    Added docstring and optional RECOMPILE arg to (defun config-load)

--- init.el
+++ init.el
@@ -8,14 +8,26 @@
 (setq custom-file (concat user-emacs-directory "custom.el"))
 
 ;; Define a function that loads my config file
-(defun config-load ()
-  (interactive)
+(defun config-load (&optional recompile)
+  "Load my config file.
+If config.elc exists, load it. 
+If config.el exists, load it, then compile it.
+If config.org exists, tangle it, load it, compile it.
+If arg RECOMPILE is non-nil, or if called interactively, do the last one"
+  (interactive '(t))
   (let* ((dir (expand-file-name "lisp/" user-emacs-directory))
 	 (el (expand-file-name "config.el" dir))
 	 (elc (expand-file-name "config.elc" dir))
 	 (org (expand-file-name "config.org" user-emacs-directory)))
-    (cond ((file-exists-p elc) (load-file elc))
-	  ((file-exists-p el) (load-file el))
+    (cond ((file-exists-p elc)
+	   (progn (when recompile
+		    (delete-file elc)
+		    (delete-file el)
+		    (config-load))
+		  (load-file elc)))
+	  ((file-exists-p el)
+	   (progn (load-file el)
+		  (byte-compile-file el)))
 	  ((file-exists-p org)
 	   (progn (org-babel-tangle-file org)
 		  (config-load))))))
