cad74309cbe0829a585d4756bb8aece4c5f490b3
Jamie Beardslee <beardsleejamie@gmail.com>
2020-03-03 18:45:37 +1300

    Extra functions now live in lisp/my-misc-defuns.el

--- lisp/my-misc-defuns.el
+++ lisp/my-misc-defuns.el
@@ -1,3 +1,5 @@
+;;; Emacs is sorely missing an interface for "apropos".  This is my
+;;; meagre attempt at makeing it useful within emacs.
 (defun system-apropos (search &optional args)
   "Run the \"apropos\" comamnd with search term SEARCH and
 optional arguments ARGS."
@@ -5,8 +7,7 @@ optional arguments ARGS."
 		     (when current-prefix-arg
 		       (read-string "apropos arguments: "))))
   (let* ((command (or (executable-find "apropos")
-		      (executable-find "man")
-		      (user-error "Man or apropos must be installed")))
+		      (user-error "apropos must be installed, usually packaged with man")))
 	 (buffer-name (format "*System Apropos %s*" search))
 	 (buffer (or (get-buffer buffer-name)
 		     (generate-new-buffer buffer-name))))
@@ -15,4 +16,53 @@ optional arguments ARGS."
        (shell-command-to-string (concat command " " args " " search))))
     (switch-to-buffer buffer)))
 
+
+;;; These two list-* functions open up a dired buffer with a list of
+;;; videos/documents.  The package `openwith' might be nice, but I just
+;;; use helm to open files externally.
+
+(defun list-documents (&optional dir)
+  "Using `find-dired', list all the postscript and pdf files a
+  specified directory.  If called interactively, prompt for
+  Directory. Else, DIR will default to ~/Documents/."
+  (interactive (list (read-directory-name "Find videos where: " "~/Documents/")))
+  (unless dir
+    (setq dir "~/Documents/"))
+  (find-dired dir
+              "\\( -iname \\*.ps -o -iname \\*.pdf \\)")
+  (dired-hide-details-mode t)
+  (setq truncate-lines t))
+
+(defun list-videos (&optional dir)
+  "Using `find-dired', list all the videos a specified directory.
+  If called interactively, prompt for Directory. Else, DIR will
+  default to ~/Downloads/."
+  (interactive (list (read-directory-name "Find videos where: " "~/Downloads/")))
+  (unless dir
+    (setq dir "~/Downloads/"))
+  (find-dired dir
+              "\\( -iname \\*.mkv -o -iname \\*.avi -o -iname \\*.mp4 -o -iname \\*.webm -o -iname \\*.m4v \\)")
+  (dired-hide-details-mode t)
+  (setq truncate-lines t))
+
+
+;;; Open the pdf file with the same name as the current buffer.
+;;; Useful for typesetting programs such as LaTeX, lilypond, ox-latex,
+;;; etc.
+(defun open-pdf-of-current-file ()
+  "Opens a pdf file of the same name as the current file"
+  (interactive)
+  (find-file-other-window (concat
+                           (file-name-sans-extension buffer-file-name)
+                           ".pdf")))
+
+
+;;; Update locally stored mail with isync and then index it with notmuch.
+(defun mailsync ()
+  "Downloads new mail and adds it to the notmuch database"
+  (interactive)
+  (shell-command "mbsync -a ; notmuch new &" "*mailsync*"))
+
 (provide 'my-misc-defuns)
+
+
