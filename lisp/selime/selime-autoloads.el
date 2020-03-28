12e83cb0d790b0fcad6e1a619e99cfb103e3b828
Jamie Beardslee <beardsleejamie@gmail.com>
2020-03-29 02:26:13 +1300

    added selime-autoloads

---
+++ lisp/selime/selime-autoloads.el
@@ -0,0 +1,56 @@
+;;; selime-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "selime" "selime.el" (24191 20553 914913 584000))
+;;; Generated autoloads from selime.el
+
+(autoload 'selime-describe-function "selime" "\
+If package \"helpful\" is installed, call `helpful-callable',
+otherwise call `describe-function'" t nil)
+
+(autoload 'selime-describe-variable "selime" "\
+If package \"helpful\" is installed, call `helpful-variable',
+otherwise call `describe-variable'" t nil)
+
+(autoload 'selime-describe-symbol "selime" "\
+If package \"helpful\" is installed, call `helpful-at-point',
+otherwise call `describe-symbol' on the symbol at point" t nil)
+
+(autoload 'selime-disassemble "selime" "\
+If point is on a function, disassemble it.  Otherwise prompt
+for a function to disassemble.
+
+\(fn FUNCTION)" t nil)
+
+(autoload 'selime-macroexpand "selime" "\
+Macro expand the following sexp." t nil)
+
+(autoload 'selime-ielm "selime" "\
+Open IELM in another window.
+
+\(fn &optional BUFFER-NAME)" t nil)
+
+(autoload 'selime-mode "selime" "\
+Enable Slime-style documentation for elisp buffers.
+
+If called interactively, enable Selime mode if ARG is positive,
+and disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it if
+ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "selime" '("selime-mode-map")))
+
+;;;***
+
+(provide 'selime-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; selime-autoloads.el ends here
