0d6144319f00e1ca0aef6130be6f499208e26913
Jamie Beardslee <beardsleejamie@gmail.com>
2020-04-11 18:49:22 +1200

    [search-query.el]: Wrapped all URLs with (url-encode-url ...); wiktionary-word now has optional language argument; added autoloads

--- lisp/search-query/search-query.el
+++ lisp/search-query/search-query.el
@@ -1,43 +1,59 @@
+;;;###autoload
 (defun torrentz2-search (query)
   "Search torrentz2.eu. Called interactively, prompt for a
 QUERY."
   (interactive (list (read-string "Torrent: ")))
   (browse-url
-   (concat "https://torrentz2.eu/search?f=" query)))
+   (url-encode-url
+    (format "https://torrentz2.eu/search?f=%s" query))))
 
 (defvar tpb-mirror "thepiratebay.org" "The Pirate Bay URL")
 
+;;;###autoload
 (defun tpb-search (query)
   "Search The Pirate Bay for QUERY.  Uses `tpb-mirror' as the
 host."
   (interactive (list (read-string "Torrent: ")))
   (browse-url
-   (format "https://%s/search/%s/0/99/0" tpb-mirror query)))
+   (url-encode-url
+    (format "https://%s/search/%s/0/99/0" tpb-mirror query))))
 
+;;;###autoload
 (defun youtube-search (query)
   "Search youtube. Called interactively, prompt for a QUERY."
   (interactive (list (read-string "Youtube: ")))
   (browse-url
-   (concat "https://m.youtube.com/search?q=" query)))
+   (url-encode-url
+    (format "https://m.youtube.com/search?q=%s" query))))
 
+;;;###autoload
 (defun wikipedia-search (query &optional language)
   "Search wikipedia.  Called interactively, prompt for a QUERY.
-With prefix arg LANGUAGE, prompt for language code"
+With prefix arg LANGUAGE, prompt for language code."
   (interactive (list (read-string "Wikipedia: ")
 		     (when current-prefix-arg
 		       (read-string "Language: "))))
   (browse-url
-   (format "https://%s.wikipedia.org/wiki/Special:Search?search=%s"
-	   (or language
-	       "en")
-	   query)))
+   (url-encode-url
+    (format "https://%s.wikipedia.org/wiki/Special:Search?search=%s"
+	    (or language
+		"en")
+	    query))))
 
-(defun wiktionary-word (word)
-  "Search wiktionary for a word. With a prefix arg, prompt for a
-word, otherwise use the word at point."
+;;;###autoload
+(defun wiktionary-word (word &optional language)
+  "Search wiktionary for a word.  Called interactively, prompt
+for a WORD with the default input being the word at point.  With
+prefix arg LANGUAGE, prompt for language code."
   (interactive (list (read-string "Wiktionary: " 
-				  (thing-at-point 'word t))))
+				  (thing-at-point 'word t))
+		     (when current-prefix-arg
+		       (read-string "Language: "))))
   (browse-url
-   (concat "https://en.wiktionary.org/wiki/" word)))
+   (url-encode-url
+    (format "https://%s.wiktionary.org/wiki/%s"
+	    (or language
+		"en")
+	    word))))
 
 (provide 'search-query)
