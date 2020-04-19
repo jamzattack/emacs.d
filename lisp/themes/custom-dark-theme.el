8c020d5e285c0889cc400757d2135a327d2d7b5d
Jamie Beardslee <beardsleejamie@gmail.com>
2020-04-20 01:44:22 +1200

    [custom-dark-theme.el]: changed foreground to white
    Was slightly off-white

--- lisp/themes/custom-dark-theme.el
+++ lisp/themes/custom-dark-theme.el
@@ -1,17 +1,16 @@
 (deftheme custom-dark
   "dark theme")
 
-(let ((class '((class color) (min-colors 89)))
-      (almost-white "#FFFFD0"))
+(let ((class '((class color) (min-colors 89))))
   (custom-theme-set-faces
    'custom-dark
-   `(default ((,class (:background "black" :foreground ,almost-white))))
+   `(default ((,class (:background "black" :foreground "white"))))
    `(border-color ((,class (:inherit 'default))))
    `(fringe ((,class (:inherit 'default))))
    `(region ((,class (:background "gray15"))))
    `(line-number-current-line ((,class (:inherit 'secondary-selection))))
    `(secondary-selection ((,class (:background "gray9" :extend t))))
-   ;; `(scroll-bar ((,class (:foreground ,almost-white :background "black"))))
+   ;; `(scroll-bar ((,class (:foreground "white" :background "black"))))
    `(mode-line ((,class (:foreground "black" :background "sky blue"))))
    `(helm-source-header ((,class (:inherit 'bold))))))
 
