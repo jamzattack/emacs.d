aafba13856081891581b98ed336e6f86ea5165fc
Jamie Beardslee <beardsleejamie@gmail.com>
2020-06-07 14:42:04 +1200

    [toggle-touchpad.el] Complete rewrite
    Variables added to change the devices if used on a different computer.
    
    Functions now exist for both the touchpad and trackpoint.
    
    `toggle-touchpad' with prefix arg now disables the touchpad and
    enables the trackpoint.

--- lisp/toggle-touchpad/toggle-touchpad.el
+++ lisp/toggle-touchpad/toggle-touchpad.el
@@ -2,7 +2,8 @@
 
 ;; Copyright (C) 2019 Jamie Beardslee
 
-;; Author: Jamie Beardslee <beardslee@gmail.com>
+;; Author: Jamie Beardslee <beardsleejamie@gmail.com>
+;; Version: 2020.06.07
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -25,33 +26,102 @@
 
 ;;; Code:
 
-(defvar touchpad-disabled-p nil
+(defvar toggle-touchpad--trackpoint-disabled-p nil
   "Whether the touchpad is disabled. If the value is non-nil, the
 touchpad is disabled.")
 
-(defun disable-touchpad ()
+(defvar toggle-touchpad--touchpad-disabled-p t
+  "Whether the touchpad is disabled. If the value is non-nil, the
+touchpad is disabled.")
+
+(defvar toggle-touchpad--trackpoint-device "TPPS/2 IBM TrackPoint"
+  "The xinput device identifier for the trackpoint.
+Use \"xinput list\" to see what devices are available.")
+
+(defvar toggle-touchpad--touchpad-device "SynPS/2 Synaptics TouchPad"
+  "The xinput device identifier for the touchpad.
+Use \"xinput list\" to see what devices are available.")
+
+(defun toggle-touchpad-xinput-command (command device)
+  "Use xinput(1) to run COMMAND on DEVICE."
+  (interactive)
+  (start-process "touchpad" " *touchpad*"
+		 "xinput"
+		 command device))
+
+
+;;; Touchpad
+
+(defun toggle-touchpad-disable-touchpad ()
   "Disables the touchpad."
-  (interactive)
-  (shell-command
-   "xinput disable 10; xinput disable 11")
-  (setq touchpad-disabled-p t)
-  (message "Mouse input disabled"))
+  (toggle-touchpad-xinput-command
+   "disable" toggle-touchpad--touchpad-device)
+  (setq toggle-touchpad--touchpad-disabled-p t))
 
-(defun enable-touchpad ()
+(defun toggle-touchpad-enable-touchpad ()
   "Enables the touchpad."
-  (interactive)
-  (shell-command
-   "xinput enable 10; xinput enable 11")
-  (setq touchpad-disabled-p nil)
-  (message "Mouse input enabled"))
+  (toggle-touchpad-xinput-command
+   "enable" toggle-touchpad--touchpad-device)
+  (setq toggle-touchpad--touchpad-disabled-p nil))
 
-(defun toggle-touchpad ()
-  "Toggles the touchpad. If the variable `touchpad-disabled-p' is
-nil, enable it. Otherwise, enable it."
+;;;###autoload
+(defun toggle-touchpad-toggle-touchpad ()
+  "Toggles the touchpad."
   (interactive)
-  (if touchpad-disabled-p
-      (enable-touchpad)
-    (disable-touchpad)))
+  (if toggle-touchpad--touchpad-disabled-p
+      (toggle-touchpad-enable-touchpad)
+    (toggle-touchpad-disable-touchpad)))
+
+
+;;; Trackpoint
+
+(defun toggle-touchpad-disable-trackpoint ()
+  "Disables the trackpoint."
+  (toggle-touchpad-xinput-command
+   "disable" toggle-touchpad--trackpoint-device)
+  (setq toggle-touchpad--trackpoint-disabled-p t))
+
+(defun toggle-touchpad-enable-trackpoint ()
+  "Enables the trackpoint."
+  (toggle-touchpad-xinput-command
+   "enable" toggle-touchpad--trackpoint-device)
+  (setq toggle-touchpad--trackpoint-disabled-p nil))
+
+;;;###autoload
+(defun toggle-touchpad-toggle-trackpoint ()
+  "Toggles the trackpoint."
+  (interactive)
+  (if toggle-touchpad--trackpoint-disabled-p
+      (toggle-touchpad-enable-trackpoint)
+    (toggle-touchpad-disable-trackpoint)))
+
+
+;;; Both
+
+(defun toggle-touchpad-enable-both ()
+  "Enables both the trackpoint and the touchpad."
+  (toggle-touchpad-enable-touchpad)
+  (toggle-touchpad-enable-trackpoint))
+
+(defun toggle-touchpad-disable-both ()
+  "Disables both the trackpoint and the touchpad."
+  (toggle-touchpad-disable-touchpad)
+  (toggle-touchpad-disable-trackpoint))
+
+;;;###autoload
+(defun toggle-touchpad (&optional arg)
+  "Toggles both the trackpoint and the touchpad.
+
+With prefix arg ARG, use my preferred settings (touchpad disabled
+and trackpoint enabled)."
+  (interactive "P")
+  (cond
+   (arg (toggle-touchpad-disable-touchpad)
+	(toggle-touchpad-enable-trackpoint))
+   ((or toggle-touchpad--trackpoint-disabled-p
+	toggle-touchpad--touchpad-disabled-p)
+    (toggle-touchpad-enable-both))
+   (t (toggle-touchpad-disable-both))))
 
 (provide 'toggle-touchpad)
 
