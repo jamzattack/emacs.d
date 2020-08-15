748cb41d5783229ac2611fb24fc9e9abd708e497
Jamie Beardslee <jdb@jamzattack.xyz>
2020-08-15 13:08:29 +1200

    [themes] big cleanup
    - Most colours are now lexically defined in the let.
    - custom-dark-theme now has all the faces that custom-theme has.
    - Restart dimmer-mode when it's already enabled.

--- lisp/themes/custom-dark-theme.el
+++ lisp/themes/custom-dark-theme.el
@@ -1,20 +1,37 @@
 (deftheme custom-dark
   "dark theme")
 
-(let ((class '((class color) (min-colors 89))))
+(let ((class '((class color) (min-colors 89)))
+      (foreground "white")
+      (background "black")
+      (background2 "gray15")
+      (background3 "gray9")
+      (modeline "sky blue"))
   (custom-theme-set-faces
    'custom-dark
-   `(default ((,class (:background "black" :foreground "white"))))
-   `(border-color ((,class (:inherit 'default))))
-   `(fringe ((,class (:inherit 'default))))
-   `(region ((,class (:background "gray15"))))
+   ;; Text
+   `(default ((,class (:background ,background :foreground ,foreground :weight normal))))
    `(line-number-current-line ((,class (:inherit 'secondary-selection))))
-   `(secondary-selection ((,class (:background "gray9" :extend t))))
-   ;; `(scroll-bar ((,class (:foreground "white" :background "black"))))
-   `(mode-line ((,class (:foreground "black" :background "sky blue"))))
+   `(secondary-selection ((,class (:background ,background3 :extend t))))
    `(helm-source-header ((,class (:inherit 'bold))))
+   `(font-lock-doc-face ((,class (:slant italic :inherit font-lock-string-face))))
+   `(dired-async-mode-message ((,class (:foreground "darkred"))))
+   `(org-hide ((,class (:foreground ,background))))
+
+   ;; Not really text
+   `(tooltip ((,class (:inherit 'default))))
+   `(region ((,class (:background ,background2))))
+   `(fringe ((,class (:inherit 'default))))
+   `(mode-line ((,class (:background ,modeline :foreground "black"))))
    `(tab-bar ((,class (:inherit mode-line-inactive :box nil))))
    `(tab-bar-tab ((,class (:inherit default))))
-   `(tab-bar-tab-inactive ((,class (:inherit mode-line-inactive :box nil))))))
+   `(tab-bar-tab-inactive ((,class (:inherit mode-line-inactive :box nil))))
+   `(ruler-mode-default ((,class (:inherit header-line :box nil))))
+   ))
+
+(when (and (member 'dimmer-mode minor-mode-list)
+	   (fboundp 'dimmer-mode))
+  (dimmer-mode -1)
+  (dimmer-mode))
 
 (provide-theme 'custom-dark)
--- lisp/themes/custom-theme.el
+++ lisp/themes/custom-theme.el
@@ -2,27 +2,36 @@
   "Not much but changing the background colour to off-white")
 
 (let ((class '((class color) (min-colors 89)))
-      (almost-white "#FFFFD0"))
+      (foreground "black")
+      (background "#FFFFD0")
+      (background2 "lightgoldenrod")
+      (background3 "khaki1")
+      (modeline "sky blue"))
   (custom-theme-set-faces
    'custom
    ;; Text
-   `(default ((,class (:background ,almost-white :foreground "black" :weight normal))))
+   `(default ((,class (:background ,background :foreground ,foreground :weight normal))))
    `(line-number-current-line ((,class (:inherit 'secondary-selection))))
-   `(secondary-selection ((,class (:background "khaki1" :extend t))))
+   `(secondary-selection ((,class (:background ,background3 :extend t))))
    `(helm-source-header ((,class (:inherit 'bold))))
    `(font-lock-doc-face ((,class (:slant italic :inherit font-lock-string-face))))
    `(dired-async-mode-message ((,class (:foreground "darkred"))))
-   `(org-hide ((,class (:foreground ,almost-white))))
+   `(org-hide ((,class (:foreground ,background))))
 
    ;; Not really text
    `(tooltip ((,class (:inherit 'default))))
-   `(region ((,class (:background "lightgoldenrod2"))))
+   `(region ((,class (:background ,background2))))
    `(fringe ((,class (:inherit 'default))))
-   `(mode-line ((,class (:background "sky blue"))))
+   `(mode-line ((,class (:background ,modeline))))
    `(tab-bar ((,class (:inherit mode-line-inactive :box nil))))
    `(tab-bar-tab ((,class (:inherit default))))
    `(tab-bar-tab-inactive ((,class (:inherit mode-line-inactive :box nil))))
    `(ruler-mode-default ((,class (:inherit header-line :box nil))))
    ))
+
+(when (and (member 'dimmer-mode minor-mode-list)
+	   (fboundp 'dimmer-mode))
+  (dimmer-mode -1)
+  (dimmer-mode))
 
 (provide-theme 'custom)
