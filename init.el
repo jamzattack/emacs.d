8a134ef711e0f0b348134b07dea79e3cdb1349b0
Jamie Beardslee <jdb@jamzattack.xyz>
2020-08-24 17:00:51 +1200

    [init] Natively compile automatically if available

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
+(when (boundp 'comp-deferred-compilation)
+  (setq comp-deferred-compilation t))
+
 (with-eval-after-load 'epg-config
   (setq epg-pinentry-mode 'loopback))
 
