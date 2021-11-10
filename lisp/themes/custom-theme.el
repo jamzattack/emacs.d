187c9f4fdf197c44c9cf93eae53040bc62ae4084
Jamie Beardslee <jdb@jamzattack.xyz>
2021-11-10 13:48:04 +1300

    [custom-theme] Add header

--- lisp/themes/custom-theme.el
+++ lisp/themes/custom-theme.el
@@ -1,3 +1,31 @@
+;;; custom-theme.el --- My custom theme              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 Jamie Beardslee
+
+;; Author: Jamie Beardslee <jdb@jamzattack.xyz>
+;; Version: 2021.11.10
+;; Keywords: theme
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a very simple theme, primarily just setting some faces that
+;; I have issues with.
+
+;;; Code:
+
 (deftheme custom
   "Not much but changing the background colour to off-white")
 
